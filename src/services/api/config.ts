f5d6abdc757202bc4d3b43efe896f68b968878fb
Vishwanath koliwad <vishwanathkoliwad@gmail.com>
2024-11-30 08:55:03 +0530

    Create config.ts

---
+++ src/services/api/config.ts
@@ -0,0 +1,4 @@
+export const API_CONFIG = {
+  FORM_SUBMISSION_URL: 'https://script.google.com/macros/s/AKfycbw0N8LUbJPMQKTFbgqWs8QgTi29t-cFpOaudVjstcz3B6bsjz1-UhJvWMq9gwpwCrx7/exec',
+  TIMEOUT: 10000, // 10 seconds
+};
